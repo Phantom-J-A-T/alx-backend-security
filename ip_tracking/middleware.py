10189d2ca4faa0d4d8757d3007a55bfa25c45775
Phantom-J-A-T <ramonadedotun550@gmail.com>
2026-01-15 20:13:29 +0100

    Creating the Ip block list python file inside the Ip traking and management folder

--- ip_tracking/middleware.py
+++ ip_tracking/middleware.py
@@ -1,15 +1,23 @@
-from .models import RequestLog
+from django.http import HttpResponseForbidden
+from .models import RequestLog, BlockedIP
 
 class IPTrackingMiddleware:
-    def __init__ (self, get_response):
+    def __init__(self, get_response):
         self.get_response = get_response
-        
-    def __call__(self, request):
-        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-        if x_forwarded_for:
-            ip = x_forwarded_for.split(',')[0]
-        else:
-            ip = request.META.get('REMOTE_ADDR')
 
-        RequestLog.objects.create(ip_address=ip, path=request.path)
+    def __call__(self, request):
+        # 1. Get the IP Address
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        ip = x_forwarded_for.split(',')[0] if x_forwarded_for else request.META.get('REMOTE_ADDR')
+
+        # 2. Security Check: Is this IP blacklisted?
+        if BlockedIP.objects.filter(ip_address=ip).exists():
+            return HttpResponseForbidden("Your IP has been blocked.")
+
+        # 3. Log the valid request
+        RequestLog.objects.create(
+            ip_address=ip,
+            path=request.path
+        )
+
         return self.get_response(request)
